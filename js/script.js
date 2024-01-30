fa026552c046b5abbf2a0b68fd3863a5b1412ca0
saifulemon <463saiful0@gmail.com>
2024-01-30 15:45:24 +0600

    added header section with mobile responsive

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let showNavbar = document.querySelector('ul');
+let menuIcon = document.querySelector('.menu-icon');
+let logo = document.querySelector('.logo');
+
+// show navbar on mobile
+menuIcon.addEventListener("click", () => {
+  let shownav = showNavbar.classList.toggle("show");
+  document.body.classList.toggle("lock-scroll");
+  shownav
+    ? logo.classList.toggle("nav-show-logo")
+    : logo.classList.toggle("nav-hide-logo");
+  return shownav;
+});
